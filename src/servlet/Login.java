5be27d7761b4d4ba48eb7702115ae3e4abd29539
Takeno90 <takehiro.moro@gmail.com>
2021-03-08 19:17:57 +0900

    10章参考

---
+++ src/servlet/Login.java
@@ -0,0 +1,61 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.LoginLogic;
+import model.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// パラメータ取得
+		request.setCharacterEncoding("UTF-8");
+		String name = request.getParameter("name");
+		String pass = request.getParameter("pass");
+		//ログイン判定
+		User user = new User(name,pass);
+		LoginLogic logic = new LoginLogic();
+		boolean isLogin = logic.execute(user);
+		//ログイン成功時セッションに保存
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+		}
+		//ログイン結果画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/servlet/Main.java
@@ -0,0 +1,95 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Mutter;
+import model.PostMutterLogic;
+import model.User;
+
+/**
+ * Servlet implementation class Main
+ */
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Main() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//つぶやきリスト取得（application）
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+		//なかった場合つぶやきリストを新規作成
+		if(mutterList == null) {
+			mutterList = new ArrayList<>();
+			application.setAttribute("mutterList",mutterList);
+		}
+
+		//ログイン情報取得
+		HttpSession session = request.getSession();
+		User loginUser = (User) session.getAttribute("loginUser");
+		//未ログインの場合リダイレクト
+		if(loginUser == null) {
+			response.sendRedirect("/docoTsubuKai/");
+		}else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//パラメータ取得
+		request.setCharacterEncoding("UTF-8");
+		String text = request.getParameter("text");
+
+		//入力値チェック
+		if(text != null && text.length() != 0) {
+			//つぶやきリスト取得
+			ServletContext application = this.getServletContext();
+			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
+
+			//ユーザー情報取得
+			HttpSession session = request.getSession();
+			User loginUser = (User)session.getAttribute("loginUser");
+
+			//つぶやきリストに追加
+			Mutter mutter = new Mutter(loginUser.getName(),text);
+			PostMutterLogic postMutterLogic = new PostMutterLogic();
+			postMutterLogic.execute(mutter, mutterList);
+
+			//application scope つぶやきリスト保存
+			application.setAttribute("mutterList", mutterList);
+		}else {
+			//エラーメッセージを保存
+			request.setAttribute("errorMsg", "つぶやきが入力されていません");
+		}
+		//メイン画面にフォワード
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+}
